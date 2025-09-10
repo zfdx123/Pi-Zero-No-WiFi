4e1c47875ef370fe026ee07969a560f750e82fad
zfdx123 <2915441170@qq.com>
2025-09-10 10:56:40 +0800

    op -> lede

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,3 +7,8 @@ sed -i 's/OpenWrt/Zero/g' package/base-files/files/bin/config_generate
 sed -i 's/0.openwrt.pool.ntp.org/0.ntp.aliyun.com/g' package/base-files/files/bin/config_generate
 
 echo 'src-git mypkg https://github.com/zfdx123/packages.git;packages' >>feeds.conf.default
+
+sed -i \
+    -e 's|^src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05|#&|' \
+    -e 's|^#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10|src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-24.10|' \
+    feeds.conf.default
